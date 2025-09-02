8b5a823f3f71c95b8ee92f4bcc595d3c5bddad86
Ryan Jewik <jewik@chapman.edu>
2025-09-01 23:22:09 -0700

    connected mongoDB and beginning to populate with postgres data aggregates

---
+++ backend/populate_mongo.py
@@ -0,0 +1,77 @@
+import psycopg2
+import os, sys, json, time
+from pathlib import Path
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from dotenv import load_dotenv
+load_dotenv()
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+uri = os.getenv("MONGO_URI")
+
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+    
+    
+#user database connection with retry mechanism
+def connect_to_database(max_retries=30, delay=2):
+    """
+    Attempt to connect to the database with retry logic
+    """
+    for attempt in range(max_retries):
+        try:
+            #print(f"Attempting to connect to database (attempt {attempt + 1}/{max_retries}):")
+
+            
+            conn = psycopg2.connect(
+                database=os.getenv("DB_NAME"),
+                user=os.getenv("DB_USERNAME"),
+                password=os.getenv("DB_PASSWORD"),
+                host=os.getenv("DB_HOST"),
+                port=os.getenv("DB_PORT")
+            )
+            #print("✅ Database connection successful!")
+            return conn
+        except Exception as e:
+            print(f"❌ Error connecting to the database (attempt {attempt + 1}): {e}")
+            if attempt < max_retries - 1:
+                print(f"⏳ Retrying in {delay} seconds...")
+                time.sleep(delay)
+            else:
+                print(f"💀 Failed to connect to database after {max_retries} attempts")
+                raise e
+            
+try:
+    conn = connect_to_database()
+except Exception as e:
+    print("❌ Failed to establish database connection:", e)
+    conn = None
+    
+    
+
+
+cursor = conn.cursor()
+rikishi_id = 1
+cursor.execute("SELECT * FROM matches WHERE id = %s", (rikishi_id,))
+rows = cursor.fetchall()
+print(rows[0])
+conn.commit()
+
+cursor.execute("""
+               SELECT column_name
+               FROM information_schema.columns
+               WHERE table_schema = 'public'  -- adjust schema
+               AND table_name   = 'matches';
+                """)
+columns = cursor.fetchall()
+print(columns)
+conn.commit()
+cursor.close()
+conn.close()
